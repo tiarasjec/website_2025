4647f8face7de59e67acce22f91ee9bbf9c0a334
vyshnav <vyshnavravi599@gmail.com>
2025-02-25 20:46:52 +0530

    implement NextAuth authentication with Google provider and session management

---
+++ src/auth.ts
@@ -0,0 +1,75 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
+import NextAuth, { type DefaultSession } from "next-auth";
+import Google, { GoogleProfile } from "next-auth/providers/google";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            role: UserRole;
+        } & DefaultSession["user"];
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        role: UserRole;
+    }
+}
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.AUTH_SECRET,
+    session: {
+        strategy: "jwt",
+    },
+    pages: {
+        signIn: "/auth/signin",
+    },
+    cookies: {
+        pkceCodeVerifier: {
+            name: "next-auth.pkce.code_verifier",
+            options: {
+                httpOnly: true,
+                sameSite: "none",
+                path: "/",
+                secure: true,
+            },
+        },
+    },
+    providers: [
+        Google({
+            profile(profile: GoogleProfile) {
+                return {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                    role: (profile.role as UserRole) ?? UserRole.PARTICIPANT,
+                };
+            },
+        }),
+    ],
+    callbacks: {
+        async jwt({ token }) {
+            if (!token.sub) return token;
+            const user = await prisma.user.findUnique({
+                where: { id: token.sub },
+            });
+            if (!user) return token;
+            token.role = user.role;
+            return token;
+        },
+        session({ session, token }) {
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    role: token.role,
+                },
+            };
+        },
+    },
+});
