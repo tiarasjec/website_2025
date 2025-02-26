24c6286597efae825fa3b0e0fbe467158d6a5d92
vyshnav <vyshnavravi599@gmail.com>
2025-02-26 11:50:37 +0530

    add Toaster component for improved user notifications in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +18,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <SessionProvider>{children}</SessionProvider>
+                <SessionProvider>
+                    {children}
+                    <Toaster />
+                </SessionProvider>
             </body>
         </html>
     );
