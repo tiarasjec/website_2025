e9f0bdfd5875d70c0a06bad67182e37463056bdb
vyshnav <vyshnavravi599@gmail.com>
2025-02-26 11:50:46 +0530

    add AdminUsersTablePage component for managing user roles and displaying user data

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import Link from "next/link";
+import { CircleUser, Menu, Users, CreditCard, Coins, Calendar } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
+import { UserRole } from "@prisma/client";
+
+export default function AdminDashboardLayout({ children }: { children: React.ReactNode }) {
+    const { data: session } = useSession({
+        required: true,
+    });
+
+    if (!session) {
+        return <>Unauthorized</>;
+    }
+    if (session.user.role !== UserRole.ADMIN) {
+        return <>Forbidden</>;
+    }
+
+    return (
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <div className="hidden border-r bg-muted/40 md:block">
+                <div className="flex h-full max-h-screen flex-col gap-2">
+                    <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+                        <Link href="/" className="flex items-center gap-2 font-semibold">
+                            <span className="">Tiara 2024</span>
+                        </Link>
+                    </div>
+                    <div className="flex-1">
+                        <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                            <Link
+                                href="/admin"
+                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                            >
+                                <Users className="h-4 w-4" />
+                                Users
+                            </Link>
+                            <Link
+                                href="/admin/payments"
+                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                            >
+                                <CreditCard className="h-4 w-4" />
+                                Payments
+                            </Link>
+                            <Link
+                                href="/admin/razorpay"
+                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                            >
+                                <Coins className="h-4 w-4" />
+                                Razorpay
+                            </Link>
+                            <Link
+                                href="/admin/events"
+                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                            >
+                                <Calendar className="h-4 w-4" />
+                                Events
+                            </Link>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col">
+                <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+                                <Menu className="h-5 w-5" />
+                                <span className="sr-only">Toggle navigation menu</span>
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent side="left" className="flex flex-col">
+                            <nav className="grid gap-2 text-lg font-medium">
+                                <Link href="#" className="flex items-center gap-2 text-lg font-semibold">
+                                    <span className="sr-only">Tiara 2024</span>
+                                </Link>
+                                <Link
+                                    href="/admin"
+                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                                >
+                                    <Users className="h-5 w-5" />
+                                    Users
+                                </Link>
+                                <Link
+                                    href="/admin/payments"
+                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-foreground hover:text-foreground"
+                                >
+                                    <CreditCard className="h-5 w-5" />
+                                    Payments
+                                </Link>
+                                <Link
+                                    href="/admin/razorpay"
+                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                                >
+                                    <Coins className="h-4 w-4" />
+                                    Razorpay
+                                </Link>
+                                <Link
+                                    href="/admin/events"
+                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                                >
+                                    <Calendar className="h-4 w-4" />
+                                    Events
+                                </Link>
+                            </nav>
+                        </SheetContent>
+                    </Sheet>
+                    <div className="w-full flex-1" />
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="secondary" size="icon" className="rounded-full">
+                                {session && session.user ? (
+                                    <Image
+                                        src={session.user.image!}
+                                        alt={session.user.name!}
+                                        width={32}
+                                        height={32}
+                                        className="rounded-full"
+                                    />
+                                ) : (
+                                    <CircleUser className="h-5 w-5" />
+                                )}
+                                <span className="sr-only">Toggle user menu</span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem asChild>
+                                <Link href="/">Homepage</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem
+                                onClick={async () =>
+                                    await signOut({
+                                        callbackUrl: "/",
+                                    })
+                                }
+                            >
+                                Logout
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </header>
+                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">{children}</main>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,173 @@
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+import { useMemo, useState, useEffect } from "react";
+import { AdvancedDataTable } from "@/components/datatable";
+import { DataTableCheckBox } from "@/components/datatable/data-table-checkbox";
+import { toast } from "sonner";
+import { UserRole, User, Payment } from "@prisma/client";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal } from "lucide-react";
+
+async function updateRole({ id, role, name }: { id: string; role: Lowercase<UserRole>; name: string }) {
+    const res = await fetch(`/api/admin/${id}/make-${role}`, {
+        method: "POST",
+    });
+    if (res.status === 200) {
+        toast.success(`${name} is now a ${role}`);
+    } else {
+        toast.error("Something went wrong");
+    }
+}
+
+async function getData() {
+    const response = await fetch("/api/admin");
+    return await response.json();
+}
+
+export default function AdminUsersTablePage() {
+    const filename = "users";
+    const [data, setData] = useState<(User & { payment: Payment[] })[]>([]);
+
+    useEffect(() => {
+        getData().then(setData);
+    }, []);
+
+    const columns = useMemo<ColumnDef<User & { payment: Payment[] }>[]>(
+        () => [
+            {
+                id: "select",
+                header: ({ table }) => (
+                    <DataTableCheckBox
+                        {...{
+                            checked: table.getIsAllRowsSelected(),
+                            indeterminate: table.getIsSomeRowsSelected(),
+                            onChange: table.getToggleAllRowsSelectedHandler(),
+                        }}
+                    />
+                ),
+                cell: ({ row }) => (
+                    <DataTableCheckBox
+                        {...{
+                            checked: row.getIsSelected(),
+                            disabled: !row.getCanSelect(),
+                            indeterminate: row.getIsSomeSelected(),
+                            onChange: row.getToggleSelectedHandler(),
+                        }}
+                    />
+                ),
+                size: 20,
+            },
+            {
+                header: "Name",
+                accessorKey: "name",
+                id: "name",
+                cell: (info) => info.getValue(),
+            },
+            {
+                accessorKey: "events",
+                id: "events",
+                header: "Events",
+                meta: {
+                    filterVariant: "text",
+                },
+            },
+            {
+                accessorKey: "college",
+                id: "college",
+                header: "College",
+                meta: {
+                    filterVariant: "text",
+                },
+            },
+            {
+                accessorKey: "role",
+                id: "role",
+                header: "Role",
+                meta: {
+                    filterVariant: "select",
+                },
+            },
+            {
+                accessorKey: "contact",
+                id: "contact",
+                header: "Contact",
+                meta: {
+                    filterVariant: "text",
+                },
+            },
+            {
+                id: "actions",
+                cell: ({ row }) => {
+                    const user = row.original;
+                    return (
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="ghost" className="h-8 w-8 p-0">
+                                    <span className="sr-only">Open menu</span>
+                                    <MoreHorizontal className="h-4 w-4" />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem
+                                    onClick={() =>
+                                        updateRole({
+                                            id: user.id,
+                                            role: "admin",
+                                            name: user.name!,
+                                        })
+                                    }
+                                >
+                                    Make Admin
+                                </DropdownMenuItem>
+                                <DropdownMenuItem
+                                    onClick={() =>
+                                        updateRole({
+                                            id: user.id,
+                                            role: "coordinator",
+                                            name: user.name!,
+                                        })
+                                    }
+                                >
+                                    Make Coordinator
+                                </DropdownMenuItem>
+                                <DropdownMenuItem
+                                    onClick={() =>
+                                        updateRole({
+                                            id: user.id,
+                                            role: "participant",
+                                            name: user.name!,
+                                        })
+                                    }
+                                >
+                                    Make User
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    );
+                },
+            },
+        ],
+        []
+    );
+
+    return (
+        <AdvancedDataTable<User & { payment: Payment[] }>
+            columns={columns}
+            data={data}
+            exportProps={{
+                exportFileName: filename,
+            }}
+        />
+    );
+}
