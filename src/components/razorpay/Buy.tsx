1adb89b0ddaca83eccc7f3d27a05030be8cebcda
vyshnav <vyshnavravi599@gmail.com>
2025-02-26 19:22:32 +0530

    add Buy component for handling payments with validation checks

---
+++ src/components/razorpay/Buy.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { makePayment } from "@/lib/utils";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Teams } from "@/lib/interfaces";
+
+const Buy = (userData: {
+    events: string[];
+    teams: Teams[];
+    amount: number;
+    college: string;
+    contact: string;
+    email: string;
+    name: string;
+}) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    return (
+        <Button
+            onClick={() => {
+                setIsLoading(true);
+                makePayment({
+                    productId: null,
+                    productName: "Tiara 2025",
+                    description:
+                        "Tiara is a National-level Techno-Cultural fest, conducted for young minds aspiring to be extraordinary.",
+                    amount: userData.amount,
+                    prefilledData: {
+                        name: userData.name,
+                        email: userData.email,
+                        college: userData.college,
+                        contact: userData.contact,
+                        events: userData.events,
+                        teams: userData.teams,
+                    },
+                });
+            }}
+            disabled={
+                userData.amount === 0 ||
+                userData.college.length === 0 ||
+                userData.contact.length < 10 ||
+                userData.events.length === 0 ||
+                (userData.teams.length > 0 && userData.teams.some((team) => team.name.length === 0)) ||
+                isLoading
+                    ? true
+                    : false
+            }
+            className={`font-semibold py-2 px-4 rounded ${isLoading ? "opacity-50 cursor-not-allowed" : ""}`}
+        >
+            {isLoading ? "Loading..." : "Pay Now"}
+        </Button>
+    );
+};
+
+export default Buy;
