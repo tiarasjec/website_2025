6e18dcbbab98d8eabc4bb2a194180238ecf6489e
vyshnav <vyshnavravi599@gmail.com>
2025-02-26 11:52:15 +0530

    add export functionality for Excel data and implement fuzzy filtering for DataTable

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { FilterFn } from "@tanstack/table-core";
+import { rankItem } from "@tanstack/match-sorter-utils";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+export const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+    // Rank the item
+    const itemRank = rankItem(row.getValue(columnId), value);
+
+    // Store the itemRank info
+    addMeta({
+        itemRank,
+    });
+
+    // Return if the item should be filtered in/out
+    return itemRank.passed;
+};
