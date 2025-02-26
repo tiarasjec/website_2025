cfb27c4d2f74f789ea00c952f25dfaf834d547da
vyshnav <vyshnavravi599@gmail.com>
2025-02-26 19:20:54 +0530

    add GET endpoint for fetching users with authorization checks in admin API

---
+++ src/app/api/admin/route.ts
@@ -0,0 +1,22 @@
+import { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+import { UserRole } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
+
+export async function GET(req: NextRequest) {
+    const session = await auth();
+    if (!session) {
+        return NextResponse.json({ message: "Unauthorized", isOk: false }, { status: 401 });
+    }
+
+    if (session.user.role !== UserRole.ADMIN && session.user.role !== UserRole.SUPER_ADMIN) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+    }
+
+    const users = await prisma.user.findMany({
+        include: {
+            payment: true,
+        },
+    });
+    return NextResponse.json(users);
+}
