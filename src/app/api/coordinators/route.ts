00329041724e98947de07038ba357e55e66085ad
vyshnav <vyshnavravi599@gmail.com>
2025-02-27 20:36:34 +0530

    add API route for retrieving event registrations with authorization checks

---
+++ src/app/api/coordinators/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse, NextRequest } from "next/server";
+import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
+
+export async function GET(req: NextRequest, { params }: { params: { event: string } }) {
+    const session = await auth();
+    if (!session) {
+        return NextResponse.json({ error: "Unauthorized", isOk: false }, { status: 401 });
+    }
+    if (session.user.role === "PARTICIPANT") {
+        return NextResponse.json({ error: "Unauthorized", isOk: false }, { status: 401 });
+    }
+    const events = [];
+    const allEvents = await prisma.event.findMany({
+        select: {
+            name: true,
+        },
+    });
+    for (const event of allEvents) {
+        const typedEvent = event as { name: string };
+        const eventRegistrations = await prisma.user
+            .findMany({
+                where: {
+                    events: {
+                        has: typedEvent.name,
+                    },
+                },
+            })
+            .then((users) => users.length);
+        events.push({ name: typedEvent.name, registrations: eventRegistrations });
+    }
+
+    return NextResponse.json(
+        events.sort((a, b) => a.name.localeCompare(b.name)),
+        { status: 200 }
+    );
+}
