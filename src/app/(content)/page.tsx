21c9b6e310a3abd93699e039b825f4035bed6c44
roche-jeethan <jeethanroche@gmail.com>
2025-02-28 15:52:49 +0530

    insert/page&loading.tsx: added a sample template home route

---
+++ src/app/(content)/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home(){
+    return (
+        <div className="flex h-screen w-full items-center justify-center">
+            <h1 className="text-3xl font-bold text-gray-500">
+                hello tiara2k25
+            </h1>
+        </div>
+    );
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+// import { tiaraFont } from "@/libs/fonts";
+// import { cn } from "@/libs/utils";
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <h1 className="text-3xl font-bold text-gray-500">
+        Loading...
+      </h1>
+    </div>
+  );
+}
