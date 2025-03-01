4fbd910f8c90145e7f9539c6fe7917df0d5a2361
adithya1770 <adithyaps929@gmail.com>
2025-03-01 14:14:54 +0530

    setting up initial layout for navbar and about

--- src/app/(content)/page.tsx
+++ src/app/(content)/page.tsx
@@ -3,6 +3,9 @@
 export default function Home(){
     return (
         <div className="flex h-screen w-full items-center justify-center">
+            <div className="absolute text-4xl top-16 right-24">
+                <a href="/menu">MENU</a>
+            </div>
             <h1 className="text-3xl font-bold text-gray-500">
                 hello tiara2k25
             </h1>
