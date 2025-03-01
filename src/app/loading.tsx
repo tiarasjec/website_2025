3ded94b6434f2f380015aee67e01bfc8fb3c867d
vyshnav <vyshnavravi599@gmail.com>
2025-03-01 08:42:57 +0530

    add Loading component for displaying a loading animation

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+import { tiaraFont } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+
+export default function Loading() {
+    return (
+        <div
+            className={cn(
+                "text-6xl md:text-9xl min-h-screen flex items-center justify-center animate-pulse",
+                tiaraFont.className
+            )}
+        >
+            <span className="">Ti</span>
+            <span className="text-tiara_red">ar</span>a <span className="text-tiara_red">{"'"}</span>24{" "}
+        </div>
+    );
+}
