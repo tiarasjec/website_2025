d81bf8f03bc045385ca8ae4ba7d1ad195305a7b0
vyshnav <vyshnavravi599@gmail.com>
2025-03-01 22:52:45 +0530

    refactor dataTable store and provider for improved context management and type safety

--- src/stores/dataTableStore.ts
+++ src/stores/dataTableStore.ts
@@ -1,14 +1,22 @@
-import { create } from "zustand";
 import { createContext, useContext } from "react";
-import { TDataTableContextMenuProps, TDataTableExportProps } from "@/components/datatable";
+import { create } from "zustand";
+
+export type TDataTableExportProps = {
+    exportFileName: string;
+    excludeColumns?: string[];
+    onUserExport?: (data: any[]) => void;
+};
+
+export type TDataTableContextMenuProps = {
+    enableEdit: boolean;
+    enableDelete: boolean;
+    extra?: { [menuName: string]: (prop: any) => void };
+};
 
 export interface IDataTableStore {
     isSelecting: boolean;
     exportProps?: TDataTableExportProps;
     contextMenuProps?: TDataTableContextMenuProps;
-}
-
-export interface DataTableStoreActions {
     toggleSelection: () => void;
     setExtraProps: (
         exportProps?: TDataTableExportProps,
@@ -16,21 +24,21 @@ export interface DataTableStoreActions {
     ) => void;
 }
 
-export type DataTableStoreType = IDataTableStore & DataTableStoreActions;
-
-export const createDataTableStore = (initProps?: Partial<IDataTableStore>) =>
-    create<DataTableStoreType>((set) => ({
-        isSelecting: false,
+const createStore = (initProps?: Partial<IDataTableStore>) =>
+    create<IDataTableStore>((set) => ({
+        isSelecting: initProps?.isSelecting ?? false,
         exportProps: initProps?.exportProps,
         contextMenuProps: initProps?.contextMenuProps,
         toggleSelection: () => set((state) => ({ isSelecting: !state.isSelecting })),
         setExtraProps: (exportProps, contextMenuProps) => set({ exportProps, contextMenuProps }),
     }));
 
-export const DataTableContext = createContext<ReturnType<typeof createDataTableStore> | null>(null);
+const StoreContext = createContext<ReturnType<typeof createStore> | null>(null);
 
 export const useDataTableStore = () => {
-    const store = useContext(DataTableContext);
-    if (!store) throw new Error("Missing DataTableContext.Provider in the tree");
+    const store = useContext(StoreContext);
+    if (!store) throw new Error("Missing DataTableContext.Provider");
     return store;
 };
+
+export { StoreContext as DataTableContext, createStore as createDataTableStore };
