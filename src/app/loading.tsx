46d0c80638f19412227504f0eeaba304d0988ba8
vyshnav <vyshnavravi599@gmail.com>
2025-03-01 09:35:54 +0530

    refactor loading component to use PageLoading for improved UI consistency

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,16 +1,5 @@
-import { tiaraFont } from "@/lib/fonts";
-import { cn } from "@/lib/utils";
+import { PageLoading } from "@/components/ui/page-loading";
 
-export default function Loading() {
-    return (
-        <div
-            className={cn(
-                "text-6xl md:text-9xl min-h-screen flex items-center justify-center animate-pulse",
-                tiaraFont.className
-            )}
-        >
-            <span className="">Ti</span>
-            <span className="text-tiara_red">ar</span>a <span className="text-tiara_red">{"'"}</span>24{" "}
-        </div>
-    );
+export default function RootLoading() {
+    return <PageLoading />;
 }
