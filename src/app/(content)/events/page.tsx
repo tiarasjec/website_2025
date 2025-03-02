d1ab58b7ac8cb5c30bf89cad121a1cae42a8152f
roche-jeethan <jeethanroche@gmail.com>
2025-03-02 13:07:25 +0530

    fixed container part

--- src/app/(content)/events/page.tsx
+++ src/app/(content)/events/page.tsx
@@ -3,6 +3,8 @@ import Container from "@/components/shared/container";
 import { LinkProps } from "@/components/ui/hover/link";
 import { useEffect, useState } from "react";
 import EventWidget from "@/components/widgets/EventWidget";
+import { cn } from "@/lib/utils";
+import { tiaraFont } from "@/lib/fonts";
 
 export default function EventsPage() {
   const [categories, setCategories] = useState<LinkProps[]>([]);
@@ -18,9 +20,17 @@ export default function EventsPage() {
 
   return (
     <>
-        <section className="w-full max-w-5xl">
-          <EventWidget />
-        </section>
+     <Container className="mx-auto pt-16">
+        <div className="py-4 mx-6 sm:mx-12 space-y-4 mt-10 pt-10">
+          <h1
+            id="about"
+            className={cn("text-center text-4xl text-white", tiaraFont.className)}
+          >
+            Events categories
+          </h1>
+          <EventWidget/>
+        </div>
+      </Container>  
     </>
   );
 }
