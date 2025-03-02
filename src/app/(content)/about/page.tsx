95364092e026c23bd219bb0a72e11f5a64001524
adithya1770 <adithyaps929@gmail.com>
2025-03-02 13:39:26 +0530

    chaged the about and home route

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -1,53 +1,29 @@
 "use client";
-import React from 'react';
-import { SparklesCore } from "../../../components/ui/sparkles";
+import React from 'react'
+import ShaderVisualization from '@/app/components/Background';
+import { cn } from '@/lib/utils';
+import { tiaraFont } from '@/lib/fonts';
 import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
 
 function page() {
-  const words = `St Joseph Engineering College has come together to host a national
-        level techno-cultural fest, Tiara 2024. Tiara is a National-level
-        Techno-Cultural fest, conducted for young minds aspiring to be
-        extraordinary, that is open to all students of undergraduate level and
-        above to come and showcase their talents and represent their
-        respective institutions on the grand stage of Tiara. Our event mainly
-        aims to spread and teach the youth to explore new areas of Technology
-        and Culture to foster the nation's development. SJEC welcomes you
-        all to Tiara-2024 To unleash your potential and unlock your skills.
-        See you on 9th, 10th and 11th May 2024.`;
+  const text = `St Joseph Engineering College has come together to host a national
+      level techno-cultural fest, Tiara 2024. Tiara is a National-level
+      Techno-Cultural fest, conducted for young minds aspiring to be
+      extraordinary, that is open to all students of undergraduate level and
+      above to come and showcase their talents and represent their
+      respective institutions on the grand stage of Tiara. Our event mainly
+      aims to spread and teach the youth to explore new areas of Technology
+      and Culture to foster the nation's development. SJEC welcomes you
+      all to Tiara-2024 To unleash your potential and unlock your skills.
+      See you on 9th, 10th and 11th May 2024`
   return (
     <div>
-      <div className="h-screen w-screen bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className="md:text-7xl text-3xl mt-28 lg:text-9xl font-bold text-center text-white z-20">
-        About TIARA!
-      </h1>
-      <div className="w-[40rem] h-40 absolute top-10">
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-red-400 to-transparent h-[2px] w-3/4 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-red-500 to-transparent h-px w-3/4" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-red-800 to-transparent h-[5px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-red-800 to-transparent h-px w-1/4" />
- 
-        <SparklesCore
-          background="transparent"
-          minSize={0.4}
-          maxSize={1}
-          particleDensity={1200}
-          className="w-full h-full"
-          particleColor="#FFFFFF"
-        />
-        
-        <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-      </div>
-    <TextGenerateEffect words={words} className='ml-12 mt-10' />
-    <iframe
-    className="px-10 mb-10"
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.9520361386985!2d74.89609701022555!3d12.910804316156533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba359dfac132663%3A0xa7bf228838232d32!2sSt%20Joseph%20Engineering%20College!5e0!3m2!1sen!2sin!4v1713257369845!5m2!1sen!2sin"
-    width="80%"
-    height="450"
-    loading="lazy"
-    ></iframe>
-    </div>
+      <ShaderVisualization />
+      <h1 className={cn("text-center text-8xl text-white", tiaraFont.className)}>About TIARA</h1>
+      <TextGenerateEffect words={text} className='ml-12 mt-10' />
     </div>
   )
 }
 
 export default page
+
--- src/app/(content)/page.tsx
+++ src/app/(content)/page.tsx
@@ -1,10 +1,29 @@
 "use client";
+import TransitionLink from "../components/TransitionLink";
+import ShaderVisualization from "../components/Background";
 
 export default function Home(){
     return (
         <div className="flex h-screen w-full items-center justify-center">
-            <div className="absolute text-4xl top-16 right-24">
-                <a href="/menu">MENU</a>
+            <ShaderVisualization />
+            <div className="absolute text-4xl top-16 rounded-full">
+            <nav className="h-20 w-max pl-20 backdrop-blur-xl flex flex-row place-items-center justify-between rounded-2xl bg-white/10">
+                <div className="flex gap-2">
+                    <TransitionLink href="/" label="Home" />
+                    <TransitionLink href="/work" label="Work" />
+                    <TransitionLink href="/about" label="About" />
+                    <TransitionLink href="/rules" label="Rules" />
+                    <button className='group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md font-medium'>
+                        <div className='inline-flex h-12 translate-y-0 items-center justify-center px-6  bg-gradient-to-r dark:from-[#ff3f3f] dark:to-[#f04131] dark:text-white text-black transition duration-500 group-hover:-translate-y-[150%]'>
+                        <TransitionLink href="/register" label="Register" />
+                        </div>
+                        <div className='absolute inline-flex h-12 w-full translate-y-[100%] items-center justify-center text-neutral-50 transition duration-500 group-hover:translate-y-0'>
+                        <span className='absolute h-full w-full translate-y-full skew-y-12 scale-y-0 bg-[#ff2727] dark:bg-[#ff3333] transition duration-500 group-hover:translate-y-0 group-hover:scale-150'></span>
+                        <span className='z-10'><TransitionLink href="/rules" label="Register" /></span>
+                        </div>
+                    </button>
+                </div>
+                </nav>
             </div>
             <h1 className="text-3xl font-bold text-gray-500">
                 hello tiara2k25
