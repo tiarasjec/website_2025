e052da878379dd497c2b106da00a7f5f8de53692
vyshnav <vyshnavravi599@gmail.com>
2025-03-07 19:09:32 +0530

    feat: add teamData module with team members and roles for improved organization

---
+++ src/app/(content)/team/information/data.ts
@@ -0,0 +1,174 @@
+import { tiaraAssetsPrefix } from "@/lib/utils";
+
+interface TeamMember {
+    name: string;
+    role: string;
+    imageUrl: string;
+}
+
+export const teamData = {
+    teamMembers: [
+        {
+            name: "Roopesh",
+            role: "Faculty Convenor",
+            imageUrl: `${tiaraAssetsPrefix}/team/roopesh.avif`,
+        },
+        {
+            name: "Priya Seema Miranda",
+            role: "Faculty Convenor",
+            imageUrl: `${tiaraAssetsPrefix}/team/Priyana Meloni Dsouza.avif`,
+        },
+        {
+            name: "Uthpal",
+            role: "Student Convenor",
+            imageUrl: `${tiaraAssetsPrefix}/team/Uthpal.avif`,
+        },
+        {
+            name: "Diya Gopal",
+            role: "Student Convenor",
+            imageUrl: `${tiaraAssetsPrefix}/team/Diya Gopal.avif`,
+        },
+        {
+            name: "Suhas Karinja",
+            role: "Technical Event Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Suhas Karinja.avif`,
+        },
+        {
+            name: "Ashton Dsouza",
+            role: "Technical Event Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Ashton Dsouza.avif`,
+        },
+        {
+            name: "Pritham Akash Dsouza",
+            role: "Non Technical Event Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Pritham Akash Dsouza.avif`,
+        },
+        {
+            name: "Dayal Palaksha",
+            role: "Non Technical Event Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Dayal Palaksha.avif`,
+        },
+        {
+            name: "Sumanth K",
+            role: "Cultural Event Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Sumanth K.avif`,
+        },
+        {
+            name: "Adithya C S",
+            role: "Discipline Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Adithya C S.avif`,
+        },
+        {
+            name: "Thejas B Shetty",
+            role: "Discipline Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Thejas B Shetty.avif`,
+        },
+        {
+            name: "Vyshnav K",
+            role: "Website Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Vyshnav K.avif`,
+        },
+        {
+            name: "Joshua Albuquerque",
+            role: "App Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Joshua%20Quinthino%20Albuquerque.avif`,
+        },
+        {
+            name: "Rishanth Fernandes",
+            role: "Logistics Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Rishanth%20Keith%20Fernandes.avif`,
+        },
+        {
+            name: "Akshay Bangera",
+            role: "Logistics Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Akshay Bangera.avif`,
+        },
+        {
+            name: "Olin",
+            role: "Registration Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Olin.avif`,
+        },
+        {
+            name: "Lisha D S",
+            role: "Registration Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Lisha D S.avif`,
+        },
+        {
+            name: "Jayarama P D",
+            role: "Stage & Decoration Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Jayarama P D.avif`,
+        },
+        {
+            name: "Arush Patrao",
+            role: "Prize & Award Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Arush Patrao.avif`,
+        },
+        {
+            name: "Mohammed Luqman",
+            role: "Event Scheduling Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Mohammed Luqman.avif`,
+        },
+        {
+            name: "Dhanush Gowda",
+            role: "Sponsorship Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Dhanush Gowda.avif`,
+        },
+        {
+            name: "Aashlin Johnson",
+            role: "Sponsorship Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Aashlin Johnson.avif`,
+        },
+        {
+            name: "Priyana Meloni Dsouza",
+            role: "Publicity Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Priyana Meloni Dsouza.avif`,
+        },
+        {
+            name: "Vishak Kumble",
+            role: "Media Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Vishak Kumble.avif`,
+        },
+        {
+            name: "Dhanush D",
+            role: "Design Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Dhanush D.avif`,
+        },
+        {
+            name: "Alston",
+            role: "Design Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Alston.avif`,
+        },
+    ] as const,
+    webMembers: [
+        {
+            name: "Vyshnav K",
+            role: "Website Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Vyshnav K.avif`,
+        },
+        {
+            name: "Joshua Albuquerque",
+            role: "App Head",
+            imageUrl: `${tiaraAssetsPrefix}/team/Joshua%20Quinthino%20Albuquerque.avif`,
+        },
+        {
+            name: "Santhsim V Dsouza",
+            role: "Frontend Lead",
+            imageUrl: `${tiaraAssetsPrefix}/team/Santhsim.avif`,
+        },
+        {
+            name: "Reuben A Vinod",
+            role: "Frontend",
+            imageUrl: `${tiaraAssetsPrefix}/team/Reuben.avif`,
+        },
+        {
+            name: "Jeethan Roche",
+            role: "Frontend",
+            imageUrl: `${tiaraAssetsPrefix}/team/Jeethan.avif`,
+        },
+        {
+            name: "Adithya P S",
+            role: "Frontend",
+            imageUrl: `${tiaraAssetsPrefix}/team/Adithya.avif`,
+        },
+    ] as const,
+} as const;
