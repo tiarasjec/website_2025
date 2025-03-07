c5de9ded986399f01979e6fe3d7d150e724be3fd
vyshnav <vyshnavravi599@gmail.com>
2025-03-07 19:17:08 +0530

    feat: update RenderCheckedItemsList to exclude specific amount and simplify display text

--- src/components/ui/renderCheckedItemList.tsx
+++ src/components/ui/renderCheckedItemList.tsx
@@ -22,13 +22,13 @@ const RenderCheckedItemsList = (
         <ul className="grid gap-3">
             {checkedItems.map((item, index) => {
                 return (
-                    item.amount !== 293 && (
+                    item.amount !== 300 && (
                         <li className="flex items-center justify-between" key={item.key}>
                             <span className="text-muted-foreground">{item.name}</span>
                             <span>
                                 {"\u20B9"}
                                 {item.amount}
-                                {item.team ? "/team (Not Including GST)" : "/person (Not Including GST)"}
+                                {item.team ? "/team" : "/person"}
                             </span>
                         </li>
                     )
