1fc0761fd879c0429347d0561666d172a40e69a9
vyshnav <vyshnavravi599@gmail.com>
2025-03-08 08:14:52 +0530

    fix: correct name spelling for Suhas Karinja in team data

--- src/app/(content)/team/information/data.ts
+++ src/app/(content)/team/information/data.ts
@@ -29,7 +29,7 @@ export const teamData = {
             imageUrl: `${tiaraAssetsPrefix}/team_webp/Diya Gopal.webp`,
         },
         {
-            name: "Suhas Karinja",
+            name: "Suhas L Karinja",
             role: "Technical Event Head",
             imageUrl: `${tiaraAssetsPrefix}/team_webp/Suhas Karinja.webp`,
         },
