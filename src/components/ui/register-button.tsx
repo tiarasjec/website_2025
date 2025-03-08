87c241dadd9fa970bb6f6a19ec8de86355686a16
vyshnav <vyshnavravi599@gmail.com>
2025-03-08 08:14:36 +0530

    feat: replace button navigation with Link component for register button

--- src/components/ui/register-button.tsx
+++ src/components/ui/register-button.tsx
@@ -1,11 +1,13 @@
 import { ChevronsRight } from "lucide-react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const RegButton = () => {
     const router = useRouter();
     return (
       <>
-        <button onClick={() => router.push("/register")} className='group relative h-12 rounded-full border-2 border-[#e54c4c] bg-gradient-to-r dark:from-[#c95050] dark:to-[#eb1c2c] from-[#ff7b7b] to-[#ff1e1e] px-5 dark:text-white text-white'>
+      <Link href="/register">
+        <button className='group relative h-12 rounded-full border-2 border-[#e54c4c] bg-gradient-to-r dark:from-[#c95050] dark:to-[#eb1c2c] from-[#ff7b7b] to-[#ff1e1e] px-5 dark:text-white text-white'>
           <span className='relative inline-flex overflow-hidden'>
             <div className='translate-y-0 skew-y-0 transition duration-500 group-hover:-translate-y-[110%] group-hover:skew-y-24'>
             Register Now<ChevronsRight className='inline-block w-6 h-8' />
@@ -15,6 +17,7 @@ const RegButton = () => {
             </div>
           </span>
         </button>
+        </Link>
       </>
     );
   };
